45d4d76408a5f5a3bc680e2a29a6a04443e6d056
Bruno Dias <dias.h.bruno@gmail.com>
2017-06-29 00:55:34 -0300

    parse just import and glob imports.

--- src/import.lisp
+++ src/import.lisp
@@ -1,15 +1,34 @@
 (in-package :jsc)
 
+(defun import-terminate (stmt)
+  (or (string= stmt "from")
+      (string= stmt ";")))
+
 (defun ast-build-import-items (stream)
   (loop
      :for (ty stmt) := (multiple-value-list (token-next stream))
-     :while (and stmt (not (string= stmt "from")))
-     :collect (when (not (string= stmt ","))
-                (ast-for stream ty stmt))))
+     :while (and stmt (not (import-terminate stmt)))
+     :collect (cond
+                ((string= stmt "*")
+                 (progn
+                    (token-next stream)
+                    (multiple-value-bind (ty-value token-value)
+                        (token-next stream)
+                      `(:glob ,token-value))))
+                ((not (string= stmt ","))
+                 (ast-for stream ty stmt)))))
 
 (defun ast-build-import (stream)
   "Build the array ast with STREAM."
-  (list :import
-        (remove-if #'null
-                   (ast-build-import-items stream))
-        (car (ast-from-stream stream))))
+  (read-spaces stream)
+  (let ((ch (peek-char nil stream nil)))
+    (cond
+      ((eq ch #\")
+       (list :import-file
+             (multiple-value-bind (ty-value token-value)
+                 (token-next stream)
+               (ast-for stream ty-value token-value))))
+      (t (list :import
+               (remove-if #'null
+                          (ast-build-import-items stream))
+               (car (ast-from-stream stream)))))))
--- t/import.lisp
+++ t/import.lisp
@@ -3,6 +3,40 @@
 (def-suite :jsc-import)
 (in-suite :jsc-import)
 
-(test ast-import
+(test ast-import-name
   (let ((tst (jsc:ast-from-string "import a from \"test\";")))
-    (is (equal tst `((:import ((:ident "a")) (:str "test")))))))
+    (is (equal tst `((:import ((:ident "a"))
+                              (:str "test")))))))
+
+(test ast-import-object
+  (let ((tst (jsc:ast-from-string "import { a } from \"test\";")))
+    (is (equal tst `((:import ((:obj (((:ident "a") :scoped-var))))
+                              (:str "test")))))))
+
+(test ast-import-mixed-name-and-object
+  (let ((tst (jsc:ast-from-string "import a, { b } from \"test\";")))
+    (is (equal tst `((:import ((:ident "a")
+                               (:obj (((:ident "b") :scoped-var))))
+                              (:str "test")))))))
+
+(test ast-import-mixed-object-and-name
+  (let ((tst (jsc:ast-from-string "import { a }, b from \"test\";")))
+    (is (equal tst `((:import ((:obj (((:ident "a") :scoped-var)))
+                               (:ident "b"))
+                              (:str "test")))))))
+
+(test ast-import-just-include
+  (let ((tst (jsc:ast-from-string "import \"test\";")))
+    (is (equal tst `((:import-file (:str "test")))))))
+
+(test ast-import-glob
+  (let ((tst (jsc:ast-from-string "import * as a from \"test\";")))
+    (is (equal tst `((:import ((:glob "a")) (:str "test")))))))
+
+(test ast-import-mixed-glob-and-default
+  (let ((tst (jsc:ast-from-string "import * as a, b from \"test\";")))
+    (is (equal tst `((:import ((:glob "a") (:ident "b")) (:str "test")))))))
+
+(test ast-import-mixed-default-and-glob
+  (let ((tst (jsc:ast-from-string "import  a, * as b from \"test\";")))
+    (is (equal tst `((:import ((:ident "a") (:glob "b")) (:str "test")))))))
